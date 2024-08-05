a19255b103948301427ca830c71fdf1dbf559f39
BoggTech <113841718+BoggTech@users.noreply.github.com>
2024-08-05 03:43:15 +0100

    main: Re-prompt when we fail to load phase files

--- main.py
+++ main.py
@@ -11,20 +11,30 @@ from tkinter.messagebox import askquestion
 from tkinter.filedialog import askdirectory
 
 # Mount phase files
-# TODO: support for other platforms/changing search folder - windows only for now just for testing
-
 result = askquestion(title="Visorview",
                     message="Would you like to specify the location of your installed game files?")
 
-if result == 'yes':
-    phase_file_dir = Filename.from_os_specific(askdirectory(title='Select Folder'))
-else:
+if not result == 'yes':
     sys.exit(0)
-phase_files = ("3.5", "3", "4", "5", "5.5", "6", "7", "8", "9", "10", "11", "12", "13", "14")
 
+phase_files = ("3.5", "3", "4", "5", "5.5", "6", "7", "8", "9", "10", "11", "12", "13", "14")
 vfs = VirtualFileSystem.get_global_ptr()
-for canon in phase_files:
-    vfs.mount(phase_file_dir / Filename("phase_" + canon + ".mf"), ".", VirtualFileSystem.MF_read_only)
+
+while True:
+    phase_file_dir = Filename.from_os_specific(askdirectory(title='Select Folder'))
+    for canon in phase_files:
+        is_success = vfs.mount(phase_file_dir / Filename("phase_" + canon + ".mf"), ".", VirtualFileSystem.MF_read_only)
+        if not is_success:
+            break
+    if not is_success:
+        result = askquestion(title="Visorview",
+                    message="Failed to locate file 'phase_" + canon + ".mf'. \n" +
+                             "Would you like to select a different folder?")
+        if not result == 'yes':
+            sys.exit(0)
+    else:
+        break
+
 
 print(phase_file_dir)
 
