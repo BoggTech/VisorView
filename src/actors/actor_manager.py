5ffe7e6475afcd73a77b206b6cc1b9424338c6e9
BoggTech <113841718+BoggTech@users.noreply.github.com>
2024-08-12 16:13:29 +0100

    actor_manager: option to preserve animation between actors
    lots of general changes to support this + fixed an issue when incrementing pose, where it would use the wrong frame

--- src/actors/actor_manager.py
+++ src/actors/actor_manager.py
@@ -1,5 +1,7 @@
 from direct.showbase.ShowBaseGlobal import hidden
 from panda3d.core import NodePath
+from direct.interval.IntervalGlobal import *
+from direct.interval.ActorInterval import ActorInterval
 from direct.showbase.ShowBase import Loader
 import src.globals.visorview_globals as visorview_globals
 
@@ -27,8 +29,10 @@ class ActorManager(NodePath):
         self.__is_animation_smoothed = True
 
         # misc info
-        self.__pose_frame = {}
-        self.__pose_animation = {}
+        self.__pose_frame = {}      # used to keep track of the current frame the actor is posed on
+        self.__pose_animation = {}  # used to keep track of the actor's posed animation (get_current_anim dont work)
+        self.__looped_animation = {}  # actor intervals = .get_current_anim() isnt always accurate, store ourselves
+        self.__from_frame_sequence = None  # stores the sequence used to loop an animation from a certain frame
 
         # actor
         self.__actor = None
@@ -36,12 +40,22 @@ class ActorManager(NodePath):
         if self.__actor_data is not None:
             self.build_actor()
 
-    def build_actor(self):
+    def build_actor(self, preserve_anim=False):
         """Function that gets the name of the current cog and assembles/configures its based on parameters defined in
         visorview_globals.py.
         """
         # hide the shadow away while we work
         self.__shadow.reparent_to(hidden)
+
+        # backup animations before we switch our actor; we'll need them to preserve animations
+        parts = self.get_actor_parts()
+        looping_part_anim = self.__looped_animation
+        looping_part_frame = {}
+        if preserve_anim and parts is not None:
+            for part in parts:
+                if part in looping_part_anim:
+                    looping_part_frame[part] = self.__actor.get_current_frame(looping_part_anim[part], part)
+
         if self.__actor is not None:
             self.__actor.cleanup()
             self.__actor.remove_node()
@@ -62,12 +76,27 @@ class ActorManager(NodePath):
         self.__actor.reparent_to(self)
         self.set_animation_smoothing(self.is_animation_smoothed())
 
-        # reset pose mode for new actor parts + prevent animations playing
+        # deal with resetting pose mode; backup everything in case we want to preserve the current pose/anim
+        is_posed = self.__is_posed
+        pose_frame = self.__pose_frame
+        pose_animation = self.__pose_animation
         self.__is_posed = {}
         self.__pose_frame = {}
         self.__pose_animation = {}
-        for part in self.__actor.get_part_names():
-            self.set_pose_mode(False, part)
+
+        if preserve_anim:
+            parts = self.get_actor_parts()
+            for part in parts:
+                # bring all the relevant old data over with our new actor
+                self.__is_posed[part] = is_posed[part] if part in is_posed else False
+                self.__pose_frame[part] = pose_frame[part] if part in pose_frame else 0
+                self.__pose_animation[part] = pose_animation[part] if part in pose_animation else None
+                # make sure the actor is updated based on this info
+                if self.is_posed(part):
+                    self.set_pose_mode(True, part)
+                else:
+                    if part in looping_part_anim:
+                        self.animate(looping_part_anim[part], part, looping_part_frame[part])
 
     def set_head_visibility(self, is_head):
         """Hides/shows the actor's head based on boolean is_head (if possible)."""
@@ -137,18 +166,49 @@ class ActorManager(NodePath):
         the first part in the dictionary."""
         part = self.get_first_part() if part is None else part
 
+        # do this before we set is_posed for this part, because these functions should return based on the current
+        # state of is_posed and not the new one
+        current_animation = self.get_current_animation(part)
+        current_animation_frame = self.get_current_frame(part)
         self.__is_posed[part] = is_posed
-        current_animation = self.__actor.get_current_anim(part)
-        current_animation_frame = self.__actor.get_current_frame(current_animation, part)
 
         if self.is_posed(part):
             self.__pose_animation[part] = current_animation
             self.__pose_frame[part] = current_animation_frame
             if current_animation is not None:
-                self.__actor.pose(self.__pose_animation[part], self.__pose_frame[part], part)
+                self._pose(self.__pose_animation[part], self.__pose_frame[part], part)
         else:
             if self.get_current_animation(part) is not None:
-                self.__actor.loop(self.get_current_animation(part), partName=part)
+                self._loop(self.get_current_animation(part), part_name=part)
+
+    def _loop(self, animation, part_name=None, from_frame=None):
+        """Method that loops an animation on a given part. If no part is specified, it will use the first part in the
+        dictionary. fromFrame can be specified, and a sequence will be used to loop the animation from that frame and
+        continue on from frame zero at the next loop. This differs from the default behaviour of actor.loop, which will
+        loop endlessly from that frame."""
+        # make sure the sequence won't jarringly restart the loop
+        if self.__from_frame_sequence is not None:
+            self.__from_frame_sequence.finish()
+        if from_frame is not None:
+            self.__from_frame_sequence = Sequence(
+                self.__actor.actorInterval(animation, startFrame=from_frame, partName=part_name),
+                Func(self.__actor.loop, animation, 1, part_name),
+            )
+            self.__from_frame_sequence.start()
+        else:
+            self.__actor.loop(animation, partName=part_name)
+        # we're storing this manually as .get_current_anim isnt accurate when actorIntervals are at play.
+        self.__looped_animation[part_name] = animation
+
+    def _pose(self, animation, frame, part=None):
+        """Method that poses the actor at a given frame. If no part is specified, it will use the first part in the
+        dictionary."""
+        # make sure the sequence won't jarringly restart the loop
+        if self.__from_frame_sequence is not None:
+            self.__from_frame_sequence.finish()
+        self.__actor.pose(animation, frame, part)
+        # we're storing this manually as .get_current_anim isnt accurate when actorIntervals are at play.
+        self.__looped_animation[part] = None
 
     def is_posed(self, part=None):
         """Returns True if the actor is posed, False otherwise. If no part is specified, it will use the first
@@ -187,7 +247,7 @@ class ActorManager(NodePath):
         # framecount will be none if no animation is currently playing, this prevents a crash
         if current_anim_frame_count is not None:
             self.__pose_frame[part] = (part_frame + count) % current_anim_frame_count
-            self.__actor.pose(part_animation, part_frame, part)
+            self._pose(part_animation, self.__pose_frame[part], part)
 
     def get_current_frame(self, part=None):
         """Get the current animation frame, either from a looping animation or pose. If no part is specified, it will
@@ -210,11 +270,11 @@ class ActorManager(NodePath):
         part = self.get_first_part() if part is None else part
         return self.__actor_animations[part] if self.__actor_animations is not None else None
 
-    def animate(self, animation_name, part=None):
+    def animate(self, animation_name, part=None, from_frame=None):
         """Loops an animation on the actor. If no part is specified, it will use the first part in the dictionary."""
         part = self.get_first_part() if part is None else part
         self.set_pose_mode(False, part)
-        self.__actor.loop(animation_name, partName=part)
+        self._loop(animation_name, from_frame=from_frame, part_name=part)
 
     def set_actor_data(self, actor_data):
         """Replaces the current actor with a new one, specified by actor_data."""
@@ -223,8 +283,8 @@ class ActorManager(NodePath):
 
     def get_actor_parts(self):
         """Returns a list of the actors part names"""
-        return self.__actor.get_part_names()
+        return self.__actor.get_part_names() if self.__actor is not None else None
 
     def __get_actor_type(self):
         """Returns the actor's type as a string.'"""
-        return self.__actor_data.get_type()
+        return self.__actor_data.get_type() if self.__actor_data is not None else None
