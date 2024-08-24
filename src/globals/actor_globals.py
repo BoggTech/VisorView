5d22ef062300599fecbc95c7dbe9a71ff8054647
BoggTech <113841718+BoggTech@users.noreply.github.com>
2024-08-24 19:51:32 +0100

    actor_globals: Adjusted heights
    made club pres slightly bigger + switched all of the estimated heights to use a_size/b_size etc

--- src/globals/actor_globals.py
+++ src/globals/actor_globals.py
@@ -15,11 +15,11 @@ HEAD_PREFIX_4 = "phase_4/maps/"
 HEAD_PREFIX_3_5 = "phase_3.5/maps/"
 
 SUPERVISORS = [
-    CogActorData("Factory Foreman (Neutral)", "sell", "b", 1.148, (0.886, 0.737, 0.784, 1.0), "phase_4/models/char/ttr_m_ene_sellbotForeman.bam", is_supervisor=True),
-    CogActorData("Factory Foreman (Angry)", "sell", "b", 1.148, (0.886, 0.737, 0.784, 1.0), "phase_4/models/char/ttr_m_ene_sellbotForemanAngry.bam", is_supervisor=True),
-    CogActorData("Mint Auditor", "cash", "c", 1.378, (0.686, 0.882, 0.831, 1.0), "phase_4/models/char/ttr_m_ene_cashbotAuditor.bam", is_supervisor=True),
-    CogActorData("Office Clerk", "law", "b", 1.323, (0.722, 0.769, 0.816, 1.0), "phase_4/models/char/ttr_m_ene_lawbotClerk.bam", is_supervisor=True),
-    CogActorData("Club President", "boss", "a", 0.706, (0.950, 0.750, 0.750, 1.0), "phase_4/models/char/ttr_m_ene_bossbotClubPresident.bam", is_supervisor=True)
+    CogActorData("Factory Foreman (Neutral)", "sell", "b", 6.05 / B_SIZE, (0.886, 0.737, 0.784, 1.0), "phase_4/models/char/ttr_m_ene_sellbotForeman.bam", is_supervisor=True),
+    CogActorData("Factory Foreman (Angry)", "sell", "b", 6.05 / B_SIZE, (0.886, 0.737, 0.784, 1.0), "phase_4/models/char/ttr_m_ene_sellbotForemanAngry.bam", is_supervisor=True),
+    CogActorData("Mint Auditor", "cash", "c", 5.7 / C_SIZE, (0.686, 0.882, 0.831, 1.0), "phase_4/models/char/ttr_m_ene_cashbotAuditor.bam", is_supervisor=True),
+    CogActorData("Office Clerk", "law", "b", 7 / B_SIZE, (0.722, 0.769, 0.816, 1.0), "phase_4/models/char/ttr_m_ene_lawbotClerk.bam", is_supervisor=True),
+    CogActorData("Club President", "boss", "a", 4.65 / A_SIZE, (0.950, 0.750, 0.750, 1.0), "phase_4/models/char/ttr_m_ene_bossbotClubPresident.bam", is_supervisor=True)
 ]
 
 BOSSES = [
